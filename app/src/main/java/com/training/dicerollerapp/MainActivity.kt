affe6b82ced4845b89dfec96cf14558c79c35190
Mai Hekal <maihekal25@gmail.com>
2025-04-26 03:27:04 +0300

    Implement dice rolling logic and set up view binding
    - Implemented `rollDice()` function to generate a random number and update the dice image.
    - Added a `setOnClickListener` to the `rollButton` to trigger the `rollDice()` function.
    - Enabled view binding in `build.gradle.kts`.
    - Initialized the `binding` in `MainActivity`.
    - Changed `setContentView` to use `binding.root`.

--- app/src/main/java/com/training/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/training/dicerollerapp/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.training.dicerollerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice(){
+        val randomNumber = (1..6).random()
+        when(randomNumber){
+            1 -> binding.diceImage.setImageResource(R.drawable.dice_1)
+            2 -> binding.diceImage.setImageResource(R.drawable.dice_2)
+            3 -> binding.diceImage.setImageResource(R.drawable.dice_3)
+            4 -> binding.diceImage.setImageResource(R.drawable.dice_4)
+            5 -> binding.diceImage.setImageResource(R.drawable.dice_5)
+            6 -> binding.diceImage.setImageResource(R.drawable.dice_6)
         }
     }
 }
